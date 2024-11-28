15196167b311a15973699fb113836ff8e33fd7c9
Hideraldo Rodrigues Marciano <95453443+Hide71@users.noreply.github.com>
2024-11-28 10:03:40 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HideraMatchGame
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HideraMatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new()
+            {
+                "🐍","🐍",
+                "🐫","🐫",
+                "🐇","🐇",
+                "🐿","🐿",
+                "🐬","🐬",
+                "🐦","🐦",
+                "🐔","🐔",
+                "🦌","🦌",
+
+            };
+            Random random = new();
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+
+            }
+
+        }
+    }
+}
