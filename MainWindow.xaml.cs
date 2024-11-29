dbd5549e9729a6a492b0955e8b1ec9d5e3318c0f
Hideraldo Rodrigues Marciano <95453443+Hide71@users.noreply.github.com>
2024-11-29 11:14:37 -0300

    Jogo terminado

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,18 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
 
 namespace HideraMatchGame
 {
@@ -12,6 +25,7 @@ namespace HideraMatchGame
         DispatcherTimer timer = new();
         int tenthsOfSecondsElapsed;
         int matchesFound;
+       
         public MainWindow()
         {
             InitializeComponent();
@@ -20,9 +34,15 @@ namespace HideraMatchGame
             SetUpGame();
         }
 
-        private void Timer_Tick(object? sender, EventArgs e)
+        private void Timer_Tick(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - Play again?";
+            }
         }
 
         private void SetUpGame()
@@ -43,15 +63,22 @@ namespace HideraMatchGame
             Random random = new();
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                int index = random.Next(animalEmoji.Count);
-                string nextEmoji = animalEmoji[index];
-                textBlock.Text = nextEmoji;
-                animalEmoji.RemoveAt(index);
+                if(textBlock.Name != "timeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    int index = random.Next(animalEmoji.Count);
+                    string nextEmoji = animalEmoji[index];
+                    textBlock.Text = nextEmoji;
+                    animalEmoji.RemoveAt(index);
+                }
 
             }
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
 
         }
-        TextBlock lastTextblockClicked;
+        TextBlock lastTextBlockClicked;
         bool findingMatch = false;
         private void TextBlock_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
@@ -59,20 +86,35 @@ namespace HideraMatchGame
             if (findingMatch == false)
             {
                 textBlock.Visibility = Visibility.Hidden;
-                lastTextblockClicked = textBlock;
+                lastTextBlockClicked = textBlock;
                 findingMatch = true;
             }
-            else if (textBlock.Text == lastTextblockClicked.Text)
+            else if (textBlock.Text == lastTextBlockClicked.Text)
             {
+                matchesFound++;
                 textBlock.Visibility = Visibility.Hidden;
                 findingMatch = false;
 
             }
             else
             {
-                lastTextblockClicked.Visibility = Visibility.Visible;
+                lastTextBlockClicked.Visibility = Visibility.Visible;
                 findingMatch = false;
             }
+
+        }
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+
+        {
+
+            if (matchesFound == 8)
+
+            {
+
+                SetUpGame();
+
+            }
+
         }
     }
 }
